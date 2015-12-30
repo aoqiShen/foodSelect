5d7fdac0c9a1e4a78614741dbdfa0a732bd5e463
xiaoliang.wang <xiaoliang.wang@qunar.com>
2015-12-30 20:26:18 +0800

    Initial Commit

---
+++ applicationAllView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  applicationAllView
+//
+//  Created by 王小亮 on 15/12/30.
+//  Copyright © 2015年 王小亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ applicationAllView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  applicationAllView
+//
+//  Created by 王小亮 on 15/12/30.
+//  Copyright © 2015年 王小亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
